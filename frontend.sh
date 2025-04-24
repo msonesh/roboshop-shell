7170d23b09505f80a0b99778aa3c29b41422c227
Admin <soneshmalathkar@gmail.com>
2025-04-24 13:39:49 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -10,6 +10,6 @@ rm -rf /usr/share/nginx/html/*
 sudo curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+sudo unzip /tmp/frontend.zip
 
 systemctl restart nginx
